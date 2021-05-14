ed522c33fb533fc29777e8625ae6863dbff75907
akshayanand12 <akshay.anand12@gmail.com>
2021-05-14 23:22:08 +0800

    Initial Commit

---
+++ src/main/java/com/akshaya/swaggerhubcontracttesting/SwaggerhubContractTestingApplication.java
@@ -0,0 +1,13 @@
+package com.akshaya.swaggerhubcontracttesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerhubContractTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerhubContractTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshaya/swaggerhubcontracttesting/SwaggerhubContractTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshaya.swaggerhubcontracttesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerhubContractTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
